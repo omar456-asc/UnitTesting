1f0ac3fbd2d4b36b83239c7d74c1182739a9d63b
Habiba Alaa <habiba.elganainey@gmail.com>
2023-03-23 02:14:26 +0200

    Add User Class

--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -4,27 +4,27 @@ use PHPUnit\Framework\TestCase;
 
 
 
-require('./src/Calculator.php');
+require('./src/Factorial.php');
 class CalculatorTest extends TestCase
 {
 
     public function test_factorial()
     {
-        $clac = new Calculator;
+        $clac = new Factorial;
         // $result = $clac->factorial(5);
 
-        $this->assertEquals(120, $clac->factorial(5));
-        $this->assertEquals(1, $clac->factorial(1));
-        $this->assertEquals(2, $clac->factorial(2));
-        $this->assertEquals(6, $clac->factorial(3));
-        $this->assertEquals(24, $clac->factorial(4));
+        $this->assertEquals(120, $clac->fact(5));
+        $this->assertEquals(1, $clac->fact(1));
+        $this->assertEquals(2, $clac->fact(2));
+        $this->assertEquals(6, $clac->fact(3));
+        $this->assertEquals(24, $clac->fact(4));
 
 
-        $this->assertEquals(1, $clac->factorial(0));
+        $this->assertEquals(1, $clac->fact(0));
 
-        $this->assertNull($clac->factorial('abc'));
-        $this->assertNull($clac->factorial(-3));
-        $this->assertNull($clac->factorial(1.5));
-        $this->assertNull($clac->factorial(NULL));
+        $this->assertNull($clac->fact('abc'));
+        $this->assertNull($clac->fact(-3));
+        $this->assertNull($clac->fact(1.5));
+        $this->assertNull($clac->fact(NULL));
     }
 }
